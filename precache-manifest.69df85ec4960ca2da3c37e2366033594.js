c4dd915414030a7043d06ae1f2cb7a84c92e6ed1
Karthikeyan <52163936+karthikpt1@users.noreply.github.com>
2020-07-28 18:11:38 +0400

    Updates

---
+++ precache-manifest.69df85ec4960ca2da3c37e2366033594.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b696268696b50720e0baba9bede6f0",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "d938fc4f95ad62dc0474",
+    "url": "/shopping-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d938fc4f95ad62dc0474",
+    "url": "/shopping-app/static/js/2.e9634fa2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-app/static/js/2.e9634fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fb1d3b8d2dd536ee9d",
+    "url": "/shopping-app/static/js/main.c0a43d3a.chunk.js"
+  },
+  {
+    "revision": "a4290364a931f3240049",
+    "url": "/shopping-app/static/js/runtime-main.3e77fe51.js"
+  },
+  {
+    "revision": "1f73dc1786d0f4fd1f4b0b4fdfb3023c",
+    "url": "/shopping-app/static/media/1.1f73dc17.jpg"
+  },
+  {
+    "revision": "62a8e571f134dea5627232ef73c0c534",
+    "url": "/shopping-app/static/media/10.62a8e571.jpg"
+  },
+  {
+    "revision": "03265c2961260e85f812aebd76dbd0e9",
+    "url": "/shopping-app/static/media/11.03265c29.jpg"
+  },
+  {
+    "revision": "ed2b0b1f24a09da27fb4266e0c805c60",
+    "url": "/shopping-app/static/media/12.ed2b0b1f.jpg"
+  },
+  {
+    "revision": "d9c16892dd79ad08f694e224a4c8dad7",
+    "url": "/shopping-app/static/media/13.d9c16892.jpg"
+  },
+  {
+    "revision": "765161189d387841d84f4516c0042acb",
+    "url": "/shopping-app/static/media/2.76516118.jpg"
+  },
+  {
+    "revision": "cf16634259ed12e3a69160d073ff3a21",
+    "url": "/shopping-app/static/media/3.cf166342.jpg"
+  },
+  {
+    "revision": "471effd658dd13aec267987177fe70c9",
+    "url": "/shopping-app/static/media/4.471effd6.jpg"
+  },
+  {
+    "revision": "3adf5015059c94aa29cca45e302fbabd",
+    "url": "/shopping-app/static/media/5.3adf5015.jpg"
+  },
+  {
+    "revision": "327427ea39d1258b9bd8db569cef9669",
+    "url": "/shopping-app/static/media/6.327427ea.jpg"
+  },
+  {
+    "revision": "bcb98e54ddde149769e5bdb56ee76607",
+    "url": "/shopping-app/static/media/7.bcb98e54.jpg"
+  },
+  {
+    "revision": "f2493f4aac50dee5cb7ed8bcd207de8a",
+    "url": "/shopping-app/static/media/8.f2493f4a.jpg"
+  },
+  {
+    "revision": "9d8431f29d368ac2c5e6b76995b1f7b8",
+    "url": "/shopping-app/static/media/9.9d8431f2.jpg"
+  },
+  {
+    "revision": "4849210abfaa56649fa9452ae9befaec",
+    "url": "/shopping-app/static/media/darkChoc.4849210a.jpg"
+  },
+  {
+    "revision": "08322ab1278913f3401ae90ed3d59a58",
+    "url": "/shopping-app/static/media/milkChoc.08322ab1.jpg"
+  },
+  {
+    "revision": "a2b855814cf4bea8285384657c57ad65",
+    "url": "/shopping-app/static/media/nuts.a2b85581.jpg"
+  }
+]);
